d5776782d32016d1fec1a96a003f51ffe7e774ab
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-10-09 17:50:55 +0500

    add chat

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AuthContentComponent } from './auth-content/auth-content.component';
 import { ContentComponent } from './content/content.component';
 
 import { AxiosService } from './axios.service';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { AxiosService } from './axios.service';
     LoginFormComponent,
     WelcomeContentComponent,
     AuthContentComponent,
-    ContentComponent
+    ContentComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/auth-content/auth-content.component.ts
+++ frontend/src/app/auth-content/auth-content.component.ts
@@ -6,28 +6,28 @@ import { AxiosService } from '../axios.service';
   templateUrl: './auth-content.component.html',
   styleUrls: ['./auth-content.component.css']
 })
+
+
+
 export class AuthContentComponent {
+  isLoggedIn: boolean = false;
   data: string[] = [];
 
   constructor(private axiosService: AxiosService) {}
 
   ngOnInit(): void {
-    this.axiosService.request(
-        "GET",
-        "/messages",
-        {}).then(
-        (response) => {
-            this.data = response.data;
-        }).catch(
-        (error) => {
-            if (error.response.status === 401) {
-                this.axiosService.setAuthToken(null);
-            } else {
-                this.data = error.response.code;
-            }
-
+    this.axiosService.request("GET", "/messages", {})
+      .then((response) => {
+        this.isLoggedIn = true;  // Логин успешен
+        this.data = response.data;
+      })
+      .catch((error) => {
+        if (error.response.status === 401) {
+          this.axiosService.setAuthToken(null);
+        } else {
+          this.data = error.response.code;
         }
-    );
+      });
   }
-
 }
+
